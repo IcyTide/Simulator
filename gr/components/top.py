cf97d2f3700217dbddfc52ef7cd56b35797498d7
ango <wangbingchaofun@gmail.com>
2024-01-19 18:02:17 +0800

    1.19 commit

--- gr/components/top.py
+++
@@ -1,9 +0,0 @@
-import gradio as gr
-from gr.constant import SUPPORT_SCHOOL
-
-
-def build_top():
-    top_components = {}
-    school_name = gr.Dropdown(choices=SUPPORT_SCHOOL, label="心法")
-    top_components["school_name"] = school_name
-    return top_components
